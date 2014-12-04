67eae5b5d4642eb7e288b14b8dc7e83768f3b40c
Wang Bo <nanfengbobo@gmail.com>
2014-12-04 17:11:50 +0800

    Create Application.js

---
+++ examples/eds-starter/src/main/webapp/app/Application.js
@@ -0,0 +1,11 @@
+Ext.define('App.Application', {
+	extend : 'Ext.app.Application',
+	name : 'App',
+	constructor : function(config) {
+		var me = this;
+		me.callParent(arguments);
+	},
+	launch : function() {
+    
+	}
+});
