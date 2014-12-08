84b0bb275a31cf9e1f6d1e28406cf8deae61e81e
Wang Bo <nanfengbobo@gmail.com>
2014-12-08 16:21:56 +0800

    Update Main.js

--- examples/eds-starter/src/main/webapp/app/view/main/Main.js
+++ examples/eds-starter/src/main/webapp/app/view/main/Main.js
@@ -1,16 +1,18 @@
 Ext.define('App.view.main.Main', {
 	extend : 'Ext.container.Container',
+	requires : [ 'App.view.main.Header' ],
 	controller : {
 		xclass : 'App.view.main.MainController'
 	},
 	layout : 'border',
 	items : [ {
-		html : 'header',
+		xtype : 'appHeader',
 		region : 'north',
 		split : false,
 		height : 50
 	}, {
 		region : 'west',
+		width : 240,
 		title : 'Navigation',
 		split : true
 	}, {
