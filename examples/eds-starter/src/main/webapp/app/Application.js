397ca7d5d8f35f10acdafa1d7f7dee4a2a574848
Wang Bo <nanfengbobo@gmail.com>
2014-12-08 15:41:03 +0800

    Update Application.js

--- examples/eds-starter/src/main/webapp/app/Application.js
+++ examples/eds-starter/src/main/webapp/app/Application.js
@@ -6,20 +6,25 @@ Ext.define('App.Application', {
 		me.callParent(arguments);
 	},
 	/**
-	* A template method that is called when your application boots. It is called before the Application's launch function is executed so gives a hook point to run any code before your Viewport is created.
-	*/
+	 * A template method that is called when your application boots. It is
+	 * called before the Application's launch function is executed so gives a
+	 * hook point to run any code before your Viewport is created.
+	 */
 	init : function(application) {
 
 	},
-        /**
-        * Called automatically when the page has completely loaded. This is an empty function that should be overridden by each application that needs to take action on page load.
-        */
+	/**
+	 * Called automatically when the page has completely loaded. This is an
+	 * empty function that should be overridden by each application that needs
+	 * to take action on page load.
+	 */
 	launch : function() {
 
 	},
-        /**
-        * A template method like init, but called after the viewport is created. This is called after the launch method of Application is executed.
-        */
+	/**
+	 * A template method like init, but called after the viewport is created.
+	 * This is called after the launch method of Application is executed.
+	 */
 	onLaunch : function(application) {
 
 	},
