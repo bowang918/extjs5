05a1ddde74ae2e9f2edc930b1b6452deb6f45224
Wang Bo <nanfengbobo@gmail.com>
2014-12-09 15:04:41 +0800

    Update MainController.js

--- examples/eds-starter/src/main/webapp/app/view/main/MainController.js
+++ examples/eds-starter/src/main/webapp/app/view/main/MainController.js
@@ -1,3 +1,36 @@
 Ext.define('App.view.main.MainController', {
-	extend : 'Ext.app.ViewController'
+	extend : 'Ext.app.ViewController',
+	
+	onNavigationTreeSelectionchange : function(tree, selected, eOpts) {
+		if(selected && selected.length === 1){
+			var node = selected[0], view = node.data.view;
+			if (view) {
+				var tabPanel = this.lookupReference('appMainTabpanel'), treePath = node.getPath(), card = tabPanel.child('panel[treePath=' + treePath + ']');
+				if (!card) {
+					card = Ext.create(view, {
+						treePath : treePath,
+						title : node.data.text,
+						closable : true,
+						icon : node.data.icon
+					});
+					tabPanel.add(card);
+				}
+				tabPanel.setActiveTab(card);
+			}
+		}
+	},
+	onMainTabPanelTabChange : function( tabPanel, newCard, oldCard, eOpts ){
+		if(!oldCard||newCard.treePath!=oldCard.treePath){
+			var navigationTree = this.lookupReference('appNavigationTree');
+			navigationTree.suspendEvents();
+			navigationTree.selectPath(newCard.treePath);
+			navigationTree.resumeEvents();
+		}
+	},
+	onMainTabPanelRemove : function( tabPanel, component, eOpts ){
+		var navigationTree = this.lookupReference('appNavigationTree');
+		if (tabPanel.items.length === 0) {
+			navigationTree.getSelectionModel().deselectAll();
+		}
+	}
 });
